48e4e91a8777fa232f1c24f9d9a8e6e7fa842489
piotrk39 <piotrk39@gmail.com>
2023-09-12 12:29:07 +0200

    Add API test

--- src/tests/API/AddBookHappyPath.spec.ts
+++ src/tests/API/AddBookHappyPath.spec.ts
@@ -1,68 +1,82 @@
-const {test} = require('@playwright/test');
-import { CommonSteps } from '../../pages/CommonSteps';
-import { ElementsPage } from '../../pages/Elements/ElementsPage';
+import { test, expect } from '@playwright/test';
+import { TestUtil } from '../../util/TestUtil';
 
-test('Verify user can enter new data into the table', async ({ page, baseURL }) => {
-    const common = new CommonSteps(page);
-    const elements = new ElementsPage(page);
+test('Create, Add and Remove a book from the library', async ({ request }) => {
+    const baseURL = 'https://demoqa.com';
+    const userName = 'Piotr' + await TestUtil.generateRandomNumber(5);
+    let userID: string;
+    let token: string;
 
-    const firstName = 'Alden';
-    const lastName = 'Cantrell';
-    const email = 'test@test.com';
-    const age = '30';
-    const salary = '12345';
-    const department = 'QA';
-
-    const newFirstName = 'Gerimedica';
-    const newLastName = 'BV';
-
-
-    await test.step('Navigate to Elements page', async () => {
-      console.log('Navigating to Elements page menu...');
-      await common.navigate(baseURL);
-      await common.navigateToMenuByText('Elements');
-      console.log('Selecting Web Tables list menu...');
-      await elements.clickWebElementsListMenu();
-      await elements.clickAddNewRecordButton();
-    });
-
-    await test.step('Fill in the registration form', async () => {
-      console.log('Filling in the registartion form...');
-      await elements.fillInRegistrationForm(firstName, lastName, email, age, salary, department);
-      console.log('Submitting registration form...');
-      await elements.clickSubmitButton();
-    });
-
-    await test.step('Validate new employee', async () => {
-      console.log('Searching for new employee...');
-      await elements.typeToSearch(department);
-      console.log('Validating new employee row...');
-      await elements.verifyRow(1, {
-        firstName: firstName,
-        lastName: lastName,
-        age: age,
-        email: email,
-        salary: salary,
-        department: department
+    await test.step('Create new user', async () => {
+      console.log('Adding user...')
+      const response = await request.post(`${baseURL}/Account/v1/User`, {
+        data: {
+          "userName": userName,
+          "password": "123ewqA@"
+        }
       });
+    
+      try {
+        const responseBody = JSON.parse(await response.text());
+        console.log(responseBody.userID);
+        userID = responseBody.userID
+      } catch (error) {
+        console.error("Error parsing JSON response:", error);
+      }
     });
 
-    await test.step('Edit new employee entry', async () => {
-      console.log('Clicking edit button...');
-      await elements.clickEditButton();
-      console.log('Filling in the registartion form...');
-      await elements.editEntryFullName(newFirstName, newLastName);
-      console.log('Submitting registration form...');
-      await elements.clickSubmitButton();
-      console.log('Validate eddited row...');
-      await elements.typeToSearch(department);
-      await elements.verifyRow(1, {
-        firstName: newFirstName,
-        lastName: newLastName,
-        age: age,
-        email: email,
-        salary: salary,
-        department: department
+    await test.step('Authorize new user', async () => {
+      console.log('Generating Token...')
+      const response = await request.post(`${baseURL}/Account/v1/GenerateToken`, {
+        data: {
+          "userName": userName,
+          "password": "123ewqA@"
+        }
       });
+    
+      try {
+        const responseBody = JSON.parse(await response.text());
+        console.log(responseBody.token);
+        token = responseBody.token;
+      } catch (error) {
+        console.error("Error parsing JSON response:", error);
+      }
     });
+
+    await test.step('Add a book to the list', async () => {
+      console.log('Adding book...');
+      const response = await request.post(`${baseURL}/BookStore/v1/Books`, {
+        data: {
+            "userId": userID,
+            "collectionOfIsbns": [
+              {
+                "isbn": "9781449325862"
+              },
+              {
+                "isbn": "9781449331818"
+              }
+            ]
+          },
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          }
+        });
+    
+      try {
+        const responseBody = JSON.parse(await response.text());
+        console.log(responseBody);
+      } catch (error) {
+        console.error("Error parsing JSON response:", error);
+      }
+    });
+
+    // await test.step('Get book list', async () => {
+    //   console.log('Getting books...');
+    //   const response = await request.get(`${baseURL}/BookStore/v1/Books`);
+    //   expect(response.status()).toBe(200);
+      
+    //   const responseBody = JSON.parse(await response.text());
+
+    //   console.log(responseBody)
+    // });
 });
